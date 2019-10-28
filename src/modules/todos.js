6701fb49880647320bbc49eabe5d4963b50cd22a
PIl0T <limdongyoung0@gmail.com>
2019-10-28 10:51:51 +0000

    typo fix

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -48,7 +48,7 @@ const todos = handleActions(
       }),
     [TOGGLE]: (state, { payload: id }) => 
       produce(state, draft => {
-        const todo = dreaft.todos.find(todo => todo.id === id);
+        const todo = draft.todos.find(todo => todo.id === id);
         todo.done = !todo.done;
       }),
     [REMOVE]: (state, { payload: id }) => 
