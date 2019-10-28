08618151b8b184183029b18f01e059762fc3766f
PIl0T <limdongyoung0@gmail.com>
2019-10-28 06:14:41 +0000

    The simplest way to connect mapDispatchToProps

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -16,12 +15,8 @@ export default connect(
   state => ({
     number: state.counter.number,
   }),
-  dispatch => 
-    bindActionCreators(
-      {
-        increase,
-        decrease,
-      },
-      dispatch,
-    ),
+  {
+    increase,
+    decrease,
+  }
 )(CounterContainer);
