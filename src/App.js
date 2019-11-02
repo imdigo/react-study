0f01bf632d4ab64e09b8efcb9e80512ffc82dfc4
PIl0T <limdongyoung0@gmail.com>
2019-11-02 10:54:41 +0000

    Splitted code with lodable component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { useState, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import lodable from '@loadable/component';
 
-const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = lodable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
           Hi
         </p>
-        <Suspense fallback={<div>Loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
-        
+        {visible && <SplitMe />}
       </header>
     </div>
   );
