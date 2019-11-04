7db2f6b3450c7160e06690b2bf512180b5654b15
PIl0T <limdongyoung0@gmail.com>
2019-11-04 19:51:04 +0900

    Made serverside render with redux-saga

--- src/index.server.js
+++ src/index.server.js
@@ -8,8 +8,10 @@ import fs from 'fs';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from './modules';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
 import PreloadContext from './lib/PreloadContext';
+import { END } from 'redux-saga';
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
@@ -56,7 +58,12 @@ const app = express();
 const serverRender = async (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버사이드 렌더링을 해줍니다.
   const context = {};
-  const store = createStore(rootReducer, applyMiddleware(thunk));
+  
+  const sagaMiddleware = createSagaMiddleware();
+  
+  const store = createStore(rootReducer, applyMiddleware(thunk, sagaMiddleware));
+  
+  const sagaPromise = sagaMiddleware.run(rootSaga).toPromise();
   
   const preloadContext = {
     done: false,
@@ -74,7 +81,9 @@ const serverRender = async (req, res, next) => {
   );
   
   ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한번 렌더링
+  store.dispatch(END); // redux-saga의 END 액션을 발생시키면 액션을 모니터링하는 사가들이 모두 종료됨
   try {
+    await sagaPromise; // 기존에 진행중이던 사가들이 모두 끝날때까지 기다림
     await Promise.all(preloadContext.promises); // 모든 프로미스를 기다립니다.
   } catch (e) {
     return res.status(500);
@@ -97,7 +106,7 @@ app.use(serverRender);
 
 // 3000 포트로 서버를 가동합니다.
 
-app.listen(3000, () => {
-  console.log('Running');
+app.listen(5000, () => {
+  console.log('Running on port 5000');
 });
 
