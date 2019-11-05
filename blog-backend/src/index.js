6053516b6624337f69d4cba744545dc2a44b3c41
PIl0T <limdongyoung0@gmail.com>
2019-11-05 23:31:37 +0900

    Made simple blog server

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,27 +1,20 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401; // Unauthorized
-    return ;
-  }
-  await next();
-  console.log('1 END');
-});
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
-app.use(ctx => {
-  ctx.body = 'hello world';
-});
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(5000, () => {
-  console.log('Listening to port 4000');
+  console.log('Listening to port 5000');
 })
