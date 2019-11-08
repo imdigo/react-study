de72cd3927e6410ebfbd2a3e036a343473f6bbd3
PIl0T <limdongyoung0@gmail.com>
2019-11-08 23:22:47 +0900

    Made mongodb linked REST API backend

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,123 +1,96 @@
-let postId = 1; // id의 초깃값
+import Post from '../../models/post';
 
-// posts 배열 초기 데이터
-const posts = [
-  {
-    id: 1,
-    title: '제목',
-    body: '내용'
-  }
-];
-
-/* 포스트 작성
-Post /api/posts
-{ title, body }
+/*
+POST api/posts
+{
+  title: '제목',
+  body: '내용',
+  tags: ['태그1', '태그2']
+}
 */
 
-export const write = ctx => {
-  // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
-  const { title, body } = ctx.request.body;
-  postId += 1; // 기존 postId 값에 1 더하기
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
+export const write = async ctx => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch(e) {
+    ctx.throw(500, e);
+  }
 };
 
-/* 포스트 목록 조회
-GET /api/posts
+
+/*
+  GET /api/posts
 */
-export const list = ctx => {
-  ctx.body = posts;
+export const list = async ctx => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 /*
-특정 포스트 조회
-GET /api/posts/:id
+  GET /api/posts/:id
 */
-export const read = ctx => {
+
+export const read = async ctx => {
   const { id } = ctx.params;
-  // 주어진 id 값으로 포스트 찾기
-  // 파라미터로 받아온 값은 문자열 형식이니, 파라미터를 숫자로 변환하거나
-  // 비교할 p.id 값을 문자열로 변경
-  const post = posts.find(p => p.id.toString() === id);
-  // 포스트가 없으면 오류 반환
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404; // Not found
+      return ;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
 };
 
-/* 특정 포스트 제거
-DELETE /api/posts/:id
+/*
+  DELETE /api/posts/:id
 */
-export const remove = ctx => {
+
+export const remove = async ctx => {
   const { id } = ctx.params;
-  // 해당 id를 가진 posts가 몇 번재인지 확인
-  const index = posts.findIndex(p => p.id.toString() === id);
-  // 포스트 없으면 오류 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content(성공하기는 했지만 응답할 데이터가 없음)
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  // index번재 아이템 제거
-  posts.splice(index, 1);
-  ctx.status = 204; // No Content
 };
 
-/* 포스트 수정(교체)
-PUT /api/posts/:id
-{ title, body }
-*/
-export const replace = ctx => {
-  // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통재로 교체할 때 사용
-  const { id } = ctx.params;
-  // 해당 id를 가진 post 인덱스 확인
-  const index = posts.findIndex(p => p.id.toString() === id);
-  // 포스트 없으면 오류 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
+/*
+  PATCH /api/posts/:id
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정', '태그']
   }
-  // 전체 객체를 덮어 씌움
-  // 따라서 id를 제외한 기존 정보를 날리고 객체 새로 만듦
-  post[index] = {
-    id,
-    ...ctx.request.body
-  };
-  ctx.body = posts[index];
-};
+*/
 
-/* 포스트 수정(특정 필드 변경)
-PATCH /api/posts/:id
-{ title, body }
-*/
-export const update = ctx => {
-  // Patch 메서드는 주어진 필드만 교체
+export const update = async ctx => {
   const { id } = ctx.params;
-  // 해당 id를 가진 post가 몇 번째인지 확인
-  const index = posts.findIndex(p => p.id.toString() === id);
-  // 포스트 없으면 오류 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+      // false일 때에는 업데이트 되기 전의 데이터를 반환합니다. 어디로? 클라로.
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return ;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  // 기존 값에 정보 덮어씌우기
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
 };
