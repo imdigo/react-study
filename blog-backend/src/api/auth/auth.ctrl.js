322694c23aaafd7e9fb57e501b21fe4b647f134b
PIl0T <limdongyoung0@gmail.com>
2019-11-11 19:44:31 +0900

    Made auth module and wrote code about register and login

---
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -0,0 +1,93 @@
+import Joi from '@hapi/joi';
+import User from '../../models/user';
+
+
+/*
+  POST /api/auth/register
+  {
+    username: 'velopert',
+    password: 'mypass123'
+  }
+*/
+export const register = async ctx => {
+  // Request Body 검증
+  const schema = Joi.object().keys({
+    username: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(20)
+      .required(),
+    password: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return ;
+  }
+  
+  const { username, password } = ctx.request.body;
+  try {
+    // username 이미 존재하는지 확인
+    const exists = await User.findByUsername(username);
+    if (exists) {
+      ctx.status = 409; // Conflict
+      return ;
+    }
+    
+    const user = new User({
+      username,
+    });
+    await user.setPassword(password); // 비번 설정
+    await user.save(); // db에 저장
+    
+    // 응답할 데이터에서 hashedPassword 필드 제거
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+
+/*
+  POST /api/auth/login
+  {
+    usrename: 'velopert',
+    password: 'mypass123'
+  }
+*/
+export const login = async ctx => {
+  const { username, password } = ctx.request.body;
+  
+  // username, password 없으면 에러
+  if (!username || !password) {
+    ctx.status = 401; // Unauthorized
+    return ;
+  }
+  
+  try {
+    const user = await User.findByUsername(username);
+    // 계정이 존재하지 않으면 에러
+    if (!user) {
+      ctx.status = 401;
+      return ;
+    }
+    const valid = await user.checkPassword(password);
+    // 잘못된 비밀번호
+    if (!valid) {
+      ctx.status = 401;
+      return ;
+    }
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const check = async ctx => {
+  // 로그인 확인
+};
+
+export const logout = async ctx => {
+  // 로그아웃
+};
