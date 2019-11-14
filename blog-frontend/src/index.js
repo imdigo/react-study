1fd02aa1d076be8d84c67b2e442365f2a66da121
PIl0T <limdongyoung0@gmail.com>
2019-11-14 20:09:18 +0900

    Implemented all login features

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -9,11 +9,25 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddlesare from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
+import { tempSetUser, check } from './modules/user';
 
 const sagaMiddleware = createSagaMiddlesare();
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if (!user) return ; // 로그인 상태가 아니면 아무고토 안함
+    
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
+}
+
 sagaMiddleware.run(rootSaga);
+loadUser();
 
 ReactDOM.render(
   <Provider store={store}>
